ee7fb1cce422a538124cff8d4876ec5de94db97a
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-02-01 19:48:37 -0600

    heroku trblshoot

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { animals } = require('./data/animals.json');
-const PORT = process.env.PORT || 443;
+var PORT = process.env.PORT || 3001;
 const app = express();
 
 
