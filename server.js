d84cd77f943544e74e9dc96c293ee33ccec3b9a2
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-02-01 19:20:35 -0600

    updated port information

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { animals } = require('./data/animals.json');
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
